0efbb5797ab9c7a179b9be4b38e62ab4a00ef53a
Admin <admin@Admins-MacBook-Air.local>
2022-09-11 14:48:21 +0530

    fetching data from API

--- app.js
+++ app.js
@@ -18,3 +18,48 @@
 // document.querySelector('#content').innerHTML=data.
 // }
 
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('myfood');
+const mealDetailsContent = document.querySelector('.myfood-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+// event listeners
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+
+// get meal list that matches with the ingredients
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "myfood-item" data-id = "${meal.idMeal}">
+                        <div class = "myfood-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "myfood-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+}
+
+
